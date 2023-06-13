ff15d1aa6ed7a94a31f6afc7d69d460d1ce4c665
DemonGodz <dkgajelli@gmail.com>
2023-06-14 04:06:07 +0530

    Day TwentyOne Programs

---
+++ DayTwentyOne/src/com/tnsif/builtinannotations/AnnotationsDemo.java
@@ -0,0 +1,30 @@
+package com.tnsif.builtinannotations;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class AnnotationsDemo {
+
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public static void main(String[] args) {
+		
+		
+		List al=new ArrayList(); //loose Coupling
+		
+		Student s1=new Student(100,"Divakar",88.99f);
+		
+		al.add(10);
+		al.add(45.88f);
+		al.add('c');
+		al.add("Nayna");
+		al.add(s1);
+		al.add(new Student(101,"Uzer",99.9f));
+		
+		System.out.println(al);
+		
+				
+
+	}
+
+}
